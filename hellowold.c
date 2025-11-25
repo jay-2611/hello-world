cc7757f1a3fcc9a96a1ffc7c5755703a643b29d5
Jayant <choudharyjayant862@gmail.com>
2025-11-25 19:22:02 +0530

    Journey started at git hub with my first year of engineering
    hii i am jayant this my first repository in git hub with hello world

---
+++ hellowold.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+   char str[100]="Hello, World! i am jayant in git hub";
+    puts(str); // Using
+    return 0; // Return statement for main function
+    
+}
